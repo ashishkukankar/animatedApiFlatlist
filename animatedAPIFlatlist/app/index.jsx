a54e697c119c12df8794df859828270e854fe50b
ashish <ashish@ashishs-MacBook-Pro.local>
2025-06-28 04:02:06 +0530

    add animation while scrolling

--- animatedAPIFlatlist/app/index.jsx
+++ animatedAPIFlatlist/app/index.jsx
@@ -1,7 +1,9 @@
 import { faker } from "@faker-js/faker";
-import { FlatList, Image, Text, View } from "react-native";
+import { useRef, useState } from "react";
+import { Animated, Image, StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
+  const AVATAR_SIZE = 50;
   const apiData = [...Array(20).keys()].map((index) => {
     return {
       keyId: faker.string.uuid(),
@@ -13,20 +15,39 @@ export default function HomeScreen() {
     };
   });
 
+  const [data,setData] = useState(apiData)
+  const scrollAtTop = useRef(new Animated.Value(0)).current;
   return (
-    <View>
-      <FlatList
-        data={apiData}
+    <View style={style.container}>
+      <Animated.FlatList
+        data={data}
         keyExtractor={(keyId, index) => index + keyId}
-        renderItem={({ item }) => {
+        contentContainerStyle={{ margin: 5 }}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { y: scrollAtTop } } }],
+          { useNativeDriver: true }
+        )}
+        renderItem={({ item, index }) => {
+          const inputRange = [-1, 0, 90 * index, 90 * (index + 1)];
+          const scale = scrollAtTop.interpolate({
+            inputRange,
+            outputRange: [1, 1, 1, 0],
+            extrapolate: "clamp",
+          });
+
+          // const opacity = scrollAtTop.interpolate({
+          //   inputRange,
+          //   outputRange: [1, 1, 1, 0],
+          //   extrapolate: "clamp",
+          // });
           return (
-            <View
+            <Animated.View
               style={{
                 flexDirection: "row",
-                margin: 10,
                 alignItems: "center",
                 backgroundColor: "#ffffffcc",
                 shadowColor: "#000000",
+                margin: 5,
                 shadowOffset: {
                   width: 0,
                   height: 10,
@@ -34,11 +55,17 @@ export default function HomeScreen() {
                 shadowOpacity: 0.3,
                 shadowRadius: 10,
                 borderRadius: 10,
+                transform: [{ scale }],
+                
               }}
             >
               <Image
                 source={{ uri: item.avatar }}
-                style={{ width: 50, height: 50, marginRight: 20 }}
+                style={{
+                  width: AVATAR_SIZE,
+                  height: AVATAR_SIZE,
+                  marginRight: 20,
+                }}
               />
               <View>
                 <Text style={{ fontSize: 20, fontWeight: "500" }}>
@@ -50,10 +77,17 @@ export default function HomeScreen() {
                 </Text>
                 <Text>{item.jobDesc}</Text>
               </View>
-            </View>
+            </Animated.View>
           );
         }}
       />
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#2600000",
+  },
+});
